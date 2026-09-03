2b4fd5508e1decb6393d3f9b7e501458127dff40
Cássio Pereira <cassio15pereira@gmail.com>
2026-09-03 15:12:41 -0300

    Update revolucao.js

--- revolucao.js
+++ revolucao.js
@@ -228,7 +228,7 @@ window.executarCalculoRS = async function(perfilCliente, ano) {
       Ascendente: { grau_absoluto: calcularAbsoluto(ascData) },
       MC: { grau_absoluto: calcularAbsoluto(mcData) },
       Nodo_Norte: { grau_absoluto: calcularAbsoluto(planetas.NodoNorte), retro: checkRetro(planetas.NodoNorte) },
-      Sizigia: { grau_absoluto: sizigiaData && sizigiaData.grau_absoluto !== undefined ? parseFloat(sizigiaData.grau_absoluto) : 0 },
+      Sizigia: { grau_absoluto: (typeof sizigiaData !== 'undefined' && sizigiaData && sizigiaData.grau_absoluto !== undefined) ? parseFloat(sizigiaData.grau_absoluto) : 0 },
       Sol: { grau_absoluto: calcularAbsoluto(planetas.Sol), retro: false },
       Lua: { grau_absoluto: calcularAbsoluto(planetas.Lua), retro: false },
       Mercúrio: { grau_absoluto: calcularAbsoluto(planetas.Mercurio), retro: checkRetro(planetas.Mercurio) },
