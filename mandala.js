1dfaa02d5effbc5de1cdef762c1d4dbcc586d537
Cássio Pereira <cassio15pereira@gmail.com>
2026-09-11 12:33:22 -0300

    Update mandala.js

--- mandala.js
+++ mandala.js
@@ -887,6 +887,7 @@ else if (diff === 2) col = "#0ea5e9"; // Sextil (Azul claro)
 
   /* APLICA O DESVIO LATERAL GLOBAL PARA EVITAR QUALQUER SOBREPOSIÇÃO NA BORDA */
   //aplicarDesvioLateralArco(outerRingItems, 7.5);
+   outerRingItems.forEach(it => it.aShift = it.aScreen);
 
   const pR = 360;
    
